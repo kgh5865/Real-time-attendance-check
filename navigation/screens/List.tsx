4d8b9e2a6fc2a9b289f07fd7a27f1b1f1e9878c8
kms05 <149045898+SNS123123@users.noreply.github.com>
2023-11-16 15:35:44 +0900

    12

--- navigation/screens/List.tsx
+++ navigation/screens/List.tsx
@@ -1,30 +1,35 @@
 import React from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
 
-export const List = () => {
+interface AppProps {}
+
+const App: React.FC<AppProps> = () => {
   const subjects: string[] = ['캡스톤디자인1', '운영체재개론', '통신네트워크및실습'];
 
   return (
     <View style={styles.container}>
       <Text>과목을 선택하세요:</Text>
       {subjects.map((subject, index) => (
-        <Button
-          key={index}
-          title={subject}
-          onPress={() => (`선택한 과목: ${String(subject)}`)}
-        />
+        <View key={index} style={styles.buttonContainer}>
+          <Button
+            title={subject}
+            onPress={() => console.log(`선택한 과목: ${subject}`)}
+          />
+        </View>
       ))}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  buttonContainer: {
+    marginVertical: 10, // 원하는 간격으로 조절
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    buttonContainer: {
-      marginVertical: 40, // 원하는 간격으로 조절
-    },
   },
 });
+
+export default App;
