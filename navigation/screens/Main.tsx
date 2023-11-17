bb176840d1e67d64e8c7e3ae5ea78107b4832fb8
jsm52 <149045423+jsm52@users.noreply.github.com>
2023-11-17 11:24:22 +0900

    11/17

--- navigation/screens/Main.tsx
+++ navigation/screens/Main.tsx
@@ -3,7 +3,8 @@ import { View, ScrollView, StyleSheet, Alert } from 'react-native';
 import { Button, ButtonGroup, withTheme, Text } from '@rneui/themed';
 import { useNavigation, RouteProp, useRoute } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-import TouchID from 'react-native-touch-id';
+import { Auth } from 'aws-amplify';
+
 
 export type RootStackParam = {
   Main: {
@@ -12,43 +13,53 @@ export type RootStackParam = {
     stu_type: string;
   };
   List: undefined;
+  List1: undefined;
+  Main2: undefined;
 };
 
-function touchFunc(){
-  TouchID.isSupported()
-      .then((supported) => {
-        if (supported) {
-          TouchID.authenticate('지문을 스캔해주세요')
-            .then((success: boolean) => {
-              if (success) {
-                Alert.alert('지문 인식 성공!'); // 'Alert.alert'를 사용하여 경고 메시지를 표시합니다.
-              } else {
-                Alert.alert('지문 인식 실패!'); // 'Alert.alert'를 사용하여 경고 메시지를 표시합니다.
-              }
-            })
-            .catch((error: Error) => {
-              console.log('지문 인식 오류', error);
-            });
-        } else {
-          Alert.alert('이 기기에서는 지문 인식을 지원하지 않습니다.'); // 'Alert.alert'를 사용하여 경고 메시지를 표시합니다.
-        }
-      });
-}
+
 
 export const Main = () => {
   const navigation = useNavigation<NativeStackNavigationProp<RootStackParam>>();
   const route = useRoute<RouteProp<RootStackParam, 'Main'>>();
 
+
+  /*사용자 정보 가져오기 */
+  const [currentUser, setCurrentUser] = useState(null);
+
+  useEffect(() => {
+    // 컴포넌트가 마운트되었을 때, 현재 인증된 사용자 정보를 가져옴
+    fetchCurrentUser();
+  }, []);
+
+  const fetchCurrentUser = async () => {//현재 사용자 정보 가져오기
+    try {
+      const user = await Auth.currentAuthenticatedUser();
+      setCurrentUser(user);
+    } catch (error) {
+      console.log('사용자 정보를 가져오는 데 실패했습니다:', error);
+      setCurrentUser(null);
+    }
+  };
+  /*사용자 정보 가져오기 */
+
+  const func = () => {
+    if ((currentUser as any).attributes?.sub === 'c97c8114-6021-4f95-b5fe-6c03f71f3f7b') {
+      console.log('교수입니다');
+      navigation.navigate('Main2');
+    } else {
+      console.log('학생입니다.');
+      navigation.navigate('Main2');
+    }
+  };
+
   return (
     <View style={styles.contentView}>
             <Text style={styles.subHeader}>스마트 출석부</Text>
             <View style={styles.buttonsContainer}>
-  <Text style={{ color: 'black', fontSize: 20 }}>{route.params?.stu_num}</Text>
-  <Text style={{ color: 'black', fontSize: 17 }}>{route.params?.stu_name}</Text>
-  <Text style={{ color: 'black', fontSize: 17 }}>{route.params?.stu_type}</Text>
   <View style={styles.rowView}>
     <Button
-      title="출석현황"
+      title="학생용"
       loading={false}
       loadingProps={{ size: 'small', color: 'white' }}
       buttonStyle={{
@@ -67,11 +78,11 @@ export const Main = () => {
         marginLeft: 60,
       }}
 
-      onPress={() => navigation.navigate('List')}
+      onPress={() => func()}
     
     />
     <Button
-      title="쪽지보내기"
+      title="교수용"
       loading={false}
       loadingProps={{ size: 'small', color: 'white' }}
       buttonStyle={{
@@ -89,53 +100,7 @@ export const Main = () => {
         width: 100,
         marginRight: 60,
       }}
-      onPress={() => console.log('쪽지보내기')}
-    />
-  </View>
-  <View style={styles.rowView}>
-    <Button
-      title="출석"
-      loading={false}
-      loadingProps={{ size: 'small', color: 'white' }}
-      buttonStyle={{
-        backgroundColor: 'rgba(150, 190, 103, 1)',
-        borderRadius: 10,
-      }}
-      titleStyle={{
-        fontWeight: 'bold',
-        fontSize: 15,
-        paddingTop: 30,
-        paddingBottom: 30,
-      }}
-      containerStyle={{
-        height: 100,
-        width: 100,
-        marginLeft: 60,
-        marginVertical: 40, // 버튼 사이의 간격을 벌리기 위해 수정
-      }}
-      onPress={() => touchFunc()}
-    />
-    <Button
-      title="종료"
-      loading={false}
-      loadingProps={{ size: 'small', color: 'white' }}
-      buttonStyle={{
-        backgroundColor: 'rgba(110, 100, 210, 1)',
-        borderRadius: 10,
-      }}
-      titleStyle={{
-        fontWeight: 'bold',
-        fontSize: 15,
-        paddingTop: 30,
-        paddingBottom: 30,
-      }}
-      containerStyle={{
-        height: 100,
-        width: 100,
-        marginRight: 60,
-        marginVertical: 40, // 버튼 사이의 간격을 벌리기 위해 수정
-      }}
-      onPress={() => touchFunc()}
+      onPress={() => func()}
     />
   </View>
 </View>
---
+++ navigation/screens/Settings.tsx
@@ -0,0 +1,56 @@
+
+import React, { useEffect, useState } from 'react';
+import { View, ScrollView, StyleSheet, Alert } from 'react-native';
+import { Button, ButtonGroup, withTheme, Text } from '@rneui/themed';
+import { useNavigation, RouteProp, useRoute } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+
+export type RootStackParam = {
+    Logout : undefined;
+}
+
+
+export const Settings: React.FC = () => {
+  const navigation = useNavigation<NativeStackNavigationProp<RootStackParam>>();
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>설정 및 로그아웃</Text>
+      <Button
+      title="로그아웃"
+      loading={false}
+      loadingProps={{ size: 'small', color: 'white' }}
+      buttonStyle={{
+        backgroundColor: 'rgba(127, 220, 103, 1)',
+        borderRadius: 10,
+      }}
+      titleStyle={{
+        fontWeight: 'bold',
+        fontSize: 15,
+        paddingTop: 30,
+        paddingBottom: 30,
+      }}
+      containerStyle={{
+        height: 100,
+        width: 100,
+        marginRight: 60,
+      }}
+      onPress={() => navigation.navigate('Logout')}
+    /> 
+    </View>
+  );
+};
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFFFFF',
+  },
+  header: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+});
